ed77dda6eee24ba768fe47f4a08d608e953a07b0
RubensZaes <rubenszaes@gmail.com>
2020-04-27 15:33:18 -0300

    Update ItemFilme.kt

--- app/src/main/java/com/example/bollyfilmes/ItemFilme.kt
+++ app/src/main/java/com/example/bollyfilmes/ItemFilme.kt
@@ -3,10 +3,10 @@ package com.example.bollyfilmes
 import android.net.Uri
 
 data class ItemFilme (
-    val id: String,
+//    val id: String,
     val title: String,
     val description: String,
     val releaseDate: String,
-    val avaliation: Float,
-    val image: Uri
+    val avaliation: Double
+//    val image: Uri
 )
